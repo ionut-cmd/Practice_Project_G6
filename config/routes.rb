c22d3dd1cb11debdbc30860ad8702a0f5eecec3e
Taimoor-R <taimoorriz@hotmail.com>
2022-02-17 16:45:58 +0000

    Added Taimoor html page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#home'
   get 'ionut', to: 'home#ionut'
+  get 'taimoor', to: 'home#taimoor'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
