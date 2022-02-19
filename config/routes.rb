657906a4eecb2fb69d787753ef48ea615efeb170
SJ2001BJ <1773780642@qq.com>
2022-02-19 21:10:34 +0000

    Fixed the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'home#home'
   get 'ionut', to: 'home#ionut'
   get 'taimoor', to: 'home#taimoor'
-  get 'test', to: 'home\taimoor' #just for testing
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
